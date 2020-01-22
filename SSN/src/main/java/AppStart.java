14bd8502f8d50da7ac254f97759b344e65cd7aed
jeddyn <nwjaavz55>
2020-01-22 21:59:26 +0100

    switch and while deleted incorret algorithm ?

--- SSN/src/main/java/AppStart.java
+++ SSN/src/main/java/AppStart.java
@@ -1,74 +1,68 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.stream.IntStream;
+
+//zaimplementuj warunki stopu: maksymalną liczbę iteracji, maksymalny dopuszczalny błąd dla epoki,
+//dane startowe, dane związane z pojedynczym krokiem, podsumowanie procesu uczenia,
 
 public class AppStart {
 
-  private static final int STAGE = 1500;
-  private static double DATA[][][] =
-      new double[][][] {
-        {{0, 0}, {0}},
-        {{0, 1}, {1}},
-        {{1, 0}, {1}},
-        {{1, 1}, {0}}
-      };
+    //przy 100 ITERATION oraz 5 EPOCH najbliższy rezultat, ponad występuje już przetrenowanie i sieć zaczyna wariować
+    private static final int ITERATION = 100;
+    private static final int EPOCH = 5;
+    private static double DATA[][][] =
+            new double[][][]{
+                    {{1, 1}, {1}},
+                    {{1, 0}, {0}},
+                    {{0, 1}, {0}},
+                    {{0, 0}, {1}}
+            };
 
-  public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-    Network network = new Network();
-    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    boolean loop = true;
-    while (loop) {
-      System.out.println(
-          "Wpisz:\n wynik - aby zobaczyc stan uczenia,\n ucz - aby uczyc siec,\n exit - aby wyjsc\n ");
-      String command = bufferedReader.readLine();
-      switch (command) {
-        case "wynik":
-          double[] result = new double[] {0, 0, 0, 0};
-          IntStream.range(0, AppStart.DATA.length)
-              .forEach(
-                  i -> {
-                    result[i] =
-                        network
-                            .forwardPropagation(AppStart.DATA[i][0])
-                            .getNeurons()[Network.INPUT_NEURONS + Network.HIDDEN_NEURONS]
-                            .getOutput();
-                  });
-          showResult(result);
-          break;
-        case "ucz":
-          System.out.println("Uczenie...");
-          IntStream.range(0, STAGE)
-              .forEach(
-                  i -> {
-                    System.out.println("Sekwencja: " + i);
-                    IntStream.range(0, DATA.length)
-                        .forEach(
-                            j ->
-                                network
-                                    .forwardPropagation(AppStart.DATA[j][0])
-                                    .backPropagation(AppStart.DATA[j][1][0]));
-                  });
-          System.out.println("Uczenie zakonczone");
-          break;
-        case "exit":
-          loop = false;
-          break;
-      }
+        Network network = new Network();
+        for (int i = 0; i < EPOCH; i++) {
+            System.out.println("Uczenie...");
+            IntStream.range(0, ITERATION)
+                    .forEach(
+                            f -> {
+                                System.out.println("Sekwencja: " + f);
+                                IntStream.range(0, DATA.length)
+                                        .forEach(
+                                                j ->
+                                                        network
+                                                                .forwardPropagation(AppStart.DATA[j][0])
+                                                                .backPropagation(AppStart.DATA[j][1][0]));
+                            });
+            System.out.println("Zakończono epokę: " + (i+1));
+            printAfterTrainResult(network);
+        }
+
     }
-    System.exit(0);
-  }
 
-  static void showResult(double[] result) {
-    System.out.println("  Wejscie 1        Wejscie 2     Oczekiwany   Stan    ");
-    IntStream.range(0, DATA.length)
-        .forEach(
-            i -> {
-              IntStream.range(0, DATA[0][0].length)
-                  .forEach(j -> System.out.print("    " + DATA[i][0][j] + "        "));
-              System.out.print(
-                  "    " + DATA[i][1][0] + "        " + String.format("%.3f", result[i]) + "  \n");
-            });
-  }
+    private static void printAfterTrainResult(Network network) {
+        double[] result = new double[]{0, 0, 0, 0};
+        IntStream.range(0, AppStart.DATA.length)
+                .forEach(
+                        i -> {
+                            result[i] =
+                                    network
+                                            .forwardPropagation(AppStart.DATA[i][0])
+                                            .getNeurons()[Network.INPUT_NEURONS + Network.HIDDEN_NEURONS]
+                                            .getOutput();
+                        });
+
+        showResult(result);
+    }
+
+    static void showResult(double[] result) {
+        System.out.println("  Wejscie 1        Wejscie 2     Oczekiwany   Stan    ");
+        IntStream.range(0, DATA.length)
+                .forEach(
+                        i -> {
+                            IntStream.range(0, DATA[0][0].length)
+                                    .forEach(j -> System.out.print("    " + DATA[i][0][j] + "        "));
+                            System.out.print(
+                                    "    " + DATA[i][1][0] + "        " + String.format("%.3f", result[i]) + "  \n");
+                        });
+    }
 }
